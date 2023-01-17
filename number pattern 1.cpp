1486c12e21e93c83076179039a2d65a9ae68d7b8
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-01-17 15:08:38 +0530

    ,

---
+++ number pattern 1.cpp
@@ -0,0 +1,27 @@
+/* Pattern
+1 1 1 
+2 2 2 
+3 3 3 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout << i << " ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+    return 0;
+}
