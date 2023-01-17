3ee37f1ebfea8d57cbeb29c47d520fa73c084ae7
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-01-17 15:19:27 +0530

    ,

---
+++ triangular number pattern 2.cpp
@@ -0,0 +1,29 @@
+/*Pattern
+1 
+2 3 
+4 5 6 
+7 8 9 10
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    int count = 1;
+    while(row <= n){
+        int col = 1;
+        while(col<=row){
+            cout << count << " ";
+            count++;
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+    return 0;
+}
