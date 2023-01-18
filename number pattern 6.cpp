37b56dc9e4fcac66ca161e00adf19eb38e6a0984
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-01-18 12:43:44 +0530

    ,

---
+++ number pattern 6.cpp
@@ -0,0 +1,27 @@
+/*Pattern 
+1 
+2 1 
+3 2 1 
+4 3 2 1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row <= n){
+        int col = 1;
+        while(col <= row){
+            cout << row-col+1 << " ";
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+    return 0;
+}
