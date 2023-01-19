5a68604589a4a1f8b1bdf75ace43d9d56debbe04
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-01-19 18:30:20 +0530

    ,

---
+++ alphabet pattern 4.cpp
@@ -0,0 +1,28 @@
+/* Pattern
+A B C 
+B C D 
+C D E 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row <= n){
+        int col = 1;
+        
+        while(col <= n){
+            char ch = 'A' + row + col - 2;
+            cout << ch << " ";
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+    return 0;
+}
