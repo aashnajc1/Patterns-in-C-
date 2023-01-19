f801e35e29130e580449ebe1753239f08f17dc5f
Aashna J Chandrashekhar <aashnajc@gmail.com>
2023-01-19 18:41:28 +0530

    ,

---
+++ triangular alphabet pattern 1.cpp
@@ -0,0 +1,28 @@
+/* Pattern
+A 
+B B 
+C C C
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row <= n){
+        int col=1;
+        while(col <= row){
+            char ch = 'A' + row - 1;
+            cout << ch << " ";
+            ch++;
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+    return 0;
+}
